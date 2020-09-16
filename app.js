e976c91209e1b1e91eaaa7da57da350a48980fa1
Vladislav Molchanov <Molchanov96@yandex.ru>
2020-09-16 13:59:52 +0300

    Удалил лишние зависимости

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
-// var createError = require('http-errors');
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -16,7 +10,6 @@ app.use(express.urlencoded({
     extended: true
 }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 // routes
 app.use(`/user`, require(`./routes/user`));
